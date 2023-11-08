3e7251f6263cfb7642a0c550e1b5531372ab17c3
Daniela <danielapatriciabystrom@gmail.com>
2023-11-08 14:06:59 +0100

    Adding second "room" with button functions

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const start = document.getElementById('start')
 const ready = document.getElementById('ready')
 const leave = document.getElementById('leave')
 
+const firstRoom = document.getElementById('first-room')
+const sword = document.getElementById('sword')
+const slingshot = document.getElementById('slingshot')
+const weapons = []
+
 const result = document.getElementById('result')
 
 ready.addEventListener('click', function() {
@@ -12,4 +17,19 @@ ready.addEventListener('click', function() {
 leave.addEventListener('click', function() {
     start.innerHTML = '<h1>Du har avslutat spelet</h1>'
     result.innerHTML = "We're sad to see you go... Tack för att du urforskade Dungeons!"
+})
+
+sword.addEventListener('click', function() {
+    firstRoom.style.display = 'none'
+    swordRoom.style.display = 'block' 
+    addWeapon('Svärd')   
+    saveGameState()
+    document.body.style.backgroundImage = "url('/images/dragon2.jpg')"
+})
+
+slingshot.addEventListener('click', function() {
+    firstRoom.style.display = 'none'
+    slingshotRoom.style.display = 'block'
+    addWeapon('Slangbella')
+    document.body.style.backgroundImage = "url('/images/dragon2.jpg')"
 })
