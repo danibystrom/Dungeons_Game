23e584387fab8844d15f361d3d7c34f7d5494fd4
Daniela <danielapatriciabystrom@gmail.com>
2023-11-08 13:54:06 +0100

    Adding style changes and js events

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const start = document.getElementById('start')
 const ready = document.getElementById('ready')
 const leave = document.getElementById('leave')
+
+const result = document.getElementById('result')
+
+ready.addEventListener('click', function() {
+    start.innerHTML = '<h1>Snyggt! Välkommen till fängelsehålan.</h1>'
+    firstRoom.style.display = 'block'
+})
+
+leave.addEventListener('click', function() {
+    start.innerHTML = '<h1>Du har avslutat spelet</h1>'
+    result.innerHTML = "We're sad to see you go... Tack för att du urforskade Dungeons!"
+})
